6583371a1a73dd0fbc3f02aebce6c76b32843424
Javier Mena <javimena@gmail.com>
2021-10-16 04:22:17 -0700

    Don't crash when image is not found (#35)

--- src/main/java/de/maxhenkel/camera/net/MessageImageUnavailable.java
+++ src/main/java/de/maxhenkel/camera/net/MessageImageUnavailable.java
@@ -2,6 +2,7 @@ package de.maxhenkel.camera.net;
 
 import de.maxhenkel.camera.TextureCache;
 import io.netty.buffer.ByteBuf;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -23,7 +24,12 @@ public class MessageImageUnavailable implements IMessage, IMessageHandler<Messag
 
     @Override
     public IMessage onMessage(MessageImageUnavailable message, MessageContext ctx) {
-        TextureCache.instance().addImage(imgUUID, new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB));
+        try {
+            final BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+            Minecraft.getMinecraft().addScheduledTask(() -> TextureCache.instance().addImage(message.imgUUID, img));
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
