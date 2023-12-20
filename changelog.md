577a03634d91fc85b407c92cb6936e575b3f28e9
Max Henkel <max@maxhenkel.de>
2023-12-20 15:34:57 +0100

    Use shared buildscript

--- changelog.md
+++ changelog.md
@@ -1,1 +1,1 @@
-- Updated to 1.20.4
+- Fixed JEI compatibility
--- src/main/java/de/maxhenkel/camera/integration/jei/ImageCloneExtension.java
+++ src/main/java/de/maxhenkel/camera/integration/jei/ImageCloneExtension.java
@@ -9,29 +9,23 @@ import mezz.jei.api.gui.ingredient.ICraftingGridHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.category.extensions.vanilla.crafting.ICraftingCategoryExtension;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeHolder;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class ImageCloneExtension<T extends ImageCloningRecipe> implements ICraftingCategoryExtension {
-
-    private final T recipe;
-
-    public ImageCloneExtension(T recipe) {
-        this.recipe = recipe;
-    }
+public class ImageCloneExtension<T extends ImageCloningRecipe> implements ICraftingCategoryExtension<T> {
 
     @Override
-    public void setRecipe(IRecipeLayoutBuilder builder, ICraftingGridHelper craftingGridHelper, IFocusGroup focuses) {
+    public void setRecipe(RecipeHolder<T> recipeHolder, IRecipeLayoutBuilder builder, ICraftingGridHelper craftingGridHelper, IFocusGroup focuses) {
         ItemStack image = new ItemStack(Main.IMAGE.get());
         ImageData.dummy().addToImage(image);
 
-        List<ItemStack> paper = Arrays.asList(recipe.getPaper().getItems());
+        List<ItemStack> paper = Arrays.asList(recipeHolder.value().getPaper().getItems());
 
         ItemStack out = image.copy();
 
         craftingGridHelper.createAndSetInputs(builder, VanillaTypes.ITEM_STACK, Arrays.asList(List.of(image), paper), 0, 0);
         craftingGridHelper.createAndSetOutputs(builder, VanillaTypes.ITEM_STACK, List.of(out));
     }
-
 }
